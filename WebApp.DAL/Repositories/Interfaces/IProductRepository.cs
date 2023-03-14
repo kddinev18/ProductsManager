e31816c197c9a1e42dfde029adb379d5c3c42570
Калоян Динков Динев <kddinev18@codingburgas.bg>
2023-03-14 15:52:45 +0200

    Add Product repository

---
+++ WebApp.DAL/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.DAL.Models.Data;
+using WebApp.DTO;
+
+namespace WebApp.DAL.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        public bool CreateProduct(ProductRequestDTO product);
+        public Product GetById(int Id);
+        public List<Product> GetProducts(int pagingSize, int skipAmount);
+        public bool EditProduct(Product product);
+        public bool DeleteProduct(int id);
+        public bool DeleteProduct(Product product);
+    }
+}
---
+++ WebApp.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.DAL.Models.Data;
+using WebApp.DAL.Repositories.Interfaces;
+using WebApp.DTO;
+
+namespace WebApp.DAL.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ProductManagerDbContext _context;
+        public ProductRepository(ProductManagerDbContext context)
+        {
+            _context = context;
+        }
+        public bool CreateProduct(ProductRequestDTO product)
+        {
+            try
+            {
+                _context.Products.Add(new Product(product));
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduct(ProductRequestDTO product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditProduct(ProductRequestDTO product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductResponseDTO GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductResponseDTO> GetProducts(int pagingSize, int skipAmount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
