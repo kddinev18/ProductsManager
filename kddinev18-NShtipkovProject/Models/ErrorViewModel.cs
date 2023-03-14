1a131d5aca80c073f81a0e9680166579cc185e54
kddinev18 <kddinev18@SL170>
2023-03-14 14:35:48 +0200

    Add project files.

---
+++ kddinev18-NShtipkovProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kddinev18_NShtipkovProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
