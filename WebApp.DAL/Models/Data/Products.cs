bb51c05c1538bbf53ac1ae166e93c656378f721c
Калоян Динков Динев <kddinev18@codingburgas.bg>
2023-03-14 14:40:00 +0200

    Add model Product

--- WebApp.DAL/Models/Data/Products.cs
+++ WebApp.DAL/Models/Data/Products.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace WebApp.DAL.Models.Data
 {
-    public class Products
+    public class Product
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdated { get; set; }
     }
 }
