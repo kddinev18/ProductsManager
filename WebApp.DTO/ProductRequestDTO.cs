6c8dd094141c3b7c765f7123f79ec25353c833bd
Калоян Динков Динев <kddinev18@codingburgas.bg>
2023-03-15 15:35:10 +0200

    Add input handling

--- WebApp.DTO/ProductRequestDTO.cs
+++ WebApp.DTO/ProductRequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,13 @@ namespace WebApp.DTO
     public class ProductRequestDTO
     {
         public int? Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "The Name must be between 3 and 50 characters.")]
         public string Name { get; set; }
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "The Description must be between 3 and 50 characters.")]
         public string Description { get; set; }
+        [Range(0.00, 200.00, ErrorMessage = "The Price must be between 0.00 and 200.00.")]
         public double Price { get; set; }
     }
 }
